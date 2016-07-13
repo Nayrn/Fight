778387c747273367ddcc218e884d6911cf496a8a
Emily Ryan <emily.ryan@ad.aie.edu>
2016-07-13 12:36:09 +1000

    no message

---
+++ Noorootoo/Assets/CameraThing.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraThing : MonoBehaviour {
+
+    private float m_horizontal;
+    private float m_vertical;
+    public float m_speed;
+    Rigidbody rigidBody;
+
+    private CursorLockMode lockMode;
+
+    public float horizontalSpeed = 2.0f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_speed = 8.0f;
+        rigidBody = GetComponent<Rigidbody>();
+        lockMode = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        MoveTurn();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SwitchCursorMode();
+        }
+        
+        Cursor.lockState = lockMode;
+
+    }
+
+    void MoveTurn()
+    {
+        m_horizontal = Input.GetAxis("Horizontal");
+        m_vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(m_horizontal, 0.0f, m_vertical);
+        movement *= m_speed * Time.deltaTime;
+        rigidBody.transform.Translate(movement);
+
+
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, h, 0);
+    }
+
+    void SwitchCursorMode()
+    {
+        switch (Cursor.lockState)
+        {
+            case CursorLockMode.Locked:
+                lockMode = CursorLockMode.None;
+                Cursor.visible = true;
+                break;
+            case CursorLockMode.None:
+                lockMode = CursorLockMode.Locked;
+                Cursor.visible = false;
+                break;
+        }
+    }
+}
---
+++ Noorootoo/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+// walk, run && jump
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public bool isGrounded;
+    public float speed;
+    
+   
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+     
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    
+        // use input manager
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+
+    }
+}
