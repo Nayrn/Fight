ec0dd6ad3e5fa450b9ef35cde977e0729796ec1b
Lachlan Richardson <richardson.lachlan@hotmail.com>
2016-07-18 13:58:10 +1000

    Memes and current version

---
+++ Noorootoo/Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+// walk, run && jump
+public enum JoystickNum
+{
+		Joystick1,
+		Joystick2,
+		Joystick3,
+		Joystick4,
+		Keyboard
+};
+
+public class Movement : MonoBehaviour
+{
+	public Rigidbody rb;
+	public JoystickNum Joystick;
+	private Camera cam;
+
+	public bool isGrounded;
+	public float speed;
+	public float jumpSpeed;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		isGrounded = true;
+
+		cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// use input manager
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector3(Input.GetAxis(Joystick + "Horizontal") * speed, Physics.gravity.y, -Input.GetAxis(Joystick + "Vertical") * speed);
+
+		float secondsLeft = 0.3f;
+		while (secondsLeft > 0)
+		{
+			if (isGrounded == true)
+			{
+				if (Input.GetButton(Joystick + "Jump"))
+				{
+					rb.AddForce(new Vector3(0, 50, 0), ForceMode.Acceleration);
+
+					if (transform.position.y > 2.0f)
+					{
+						isGrounded = false;
+
+					}
+				}
+			}
+			secondsLeft -= Time.deltaTime;
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
+	}
+};
---
+++ Noorootoo/Assets/Scripts/PlayerValues.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerValues : MonoBehaviour {
+
+	private const float MAX_HEALTH = 100;
+
+	public float m_Health;
+	public float m_Damage;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
